eea7159069738d7ba9e69775395c43c92424a86d
PowerOleg <geraskin.oleg@mail.ru>
2024-08-03 13:31:56 +0300

    cmake has just configured.not libpqxx

---
+++ out/build/x64-Debug/libpqxx-build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/cpp/libpqxx-7.9.2/libpqxx-7.9.2/test
+# Build directory: C:/cpp/sixth/cmakeSQL_libpqxx/out/build/x64-Debug/libpqxx-build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(runner "C:/cpp/sixth/cmakeSQL_libpqxx/out/build/x64-Debug/libpqxx-build/test/runner.exe")
+set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "C:/cpp/libpqxx-7.9.2/libpqxx-7.9.2/test/CMakeLists.txt;12;add_test;C:/cpp/libpqxx-7.9.2/libpqxx-7.9.2/test/CMakeLists.txt;0;")
