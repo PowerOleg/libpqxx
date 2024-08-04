b2b9dc325e1c99e3ed3310c6f5d7cb71f5e0f5e7
PowerOleg <geraskin.oleg@mail.ru>
2024-08-04 10:35:16 +0300

    cmake.libpqxx configured

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <pqxx/pqxx>
+#include <Windows.h>
+#include <tuple>
+#pragma execution_character_set("utf-8")
+
 
 int main(int argc, char** argv)
 {
-	std::cout << "hello" << std::endl;
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	std::cout << "Program has started" << std::endl;
+	try
+	{
+		pqxx::connection conn("host=localhost port=5432 dbname=cpp_integration user=postgres password=10");
+		std::cout << "Connection is successful";
+
+		pqxx::work tx{ conn };
+		auto values = tx.query<std::string, std::string>("SELECT * FROM books");
+	
+		for (std::tuple<std::string, std::string> value : values)
+		{
+			std::cout << std::get<0>(value) << std::endl;
+		}
+	
+	
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+
+
+
 	return 0;
 }
